33ee2a32d08c7fc288e4436dd06bb2a2bb3caad1
aquakolo <aquakolo13@gmail.com>
2023-03-04 01:11:40 +0100

    Start project calculator

--- src/equation.hpp
+++ src/equation.hpp
@@ -11,7 +11,7 @@ public:
     Equation(Expression * e);
     ~Equation() = default;
 
-    friend void operator>>(Equation& e, istream& s);
+    friend void operator>>(istream& s, Equation& e);
 
     double count();
     std::shared_ptr<const Expression> structure();
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
-
+#include "equation.hpp"
 
 int main(int argc, char *argv[])
 {
-    
+    Equation e;
+    cin>>e;
+    cout<<e.expression() << " = "<<e.count();
 }
